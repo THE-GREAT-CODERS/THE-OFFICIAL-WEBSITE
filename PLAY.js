df8f274ca75e009e8cf4399cd1d980883c3acc65
Shaman07 <71549756+Shaman07@users.noreply.github.com>
2020-10-14 18:14:08 +0530

    Update PLAY.js

--- PLAY.js
+++ PLAY.js
@@ -1,1 +1,2 @@
-playSound("sound://category_background/f1_race.mp3");
+//playSound("sound://category_background/f1_race.mp3");
+console.log("HI,WE ARE THE GRAT CODERS DO YOU NEED ANY HELP.WE ARE HERE TO HELP YOU.1> IF YOU WANT TO JOIN US SCROLL DOWN");
